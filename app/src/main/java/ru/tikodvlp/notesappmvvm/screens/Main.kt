32f33a8ff2b078407e73915d61a00d234600a8a3
Tigran <tigranma@gmail.com>
2022-07-13 21:53:24 +0300

    lesson 1

---
+++ app/src/main/java/ru/tikodvlp/notesappmvvm/screens/Main.kt
@@ -0,0 +1,8 @@
+package ru.tikodvlp.notesappmvvm.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Main(navController: NavHostController) {
+}
---
+++ app/src/main/java/ru/tikodvlp/notesappmvvm/screens/Start.kt
@@ -0,0 +1,10 @@
+package ru.tikodvlp.notesappmvvm.screens
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun Start(navController: NavHostController) {
+    Text(text = "Notes App Test")
+}
