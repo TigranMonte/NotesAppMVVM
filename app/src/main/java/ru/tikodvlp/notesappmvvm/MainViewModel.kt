a49487a57898d9ce8221674b3929e7c36e691a9f
Tigran <tigranma@gmail.com>
2022-07-18 18:06:09 +0300

    Lesson 4

--- app/src/main/java/ru/tikodvlp/notesappmvvm/MainViewModel.kt
+++ app/src/main/java/ru/tikodvlp/notesappmvvm/MainViewModel.kt
@@ -6,40 +6,27 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import ru.tikodvlp.notesappmvvm.database.room.AppRoomDatabase
+import ru.tikodvlp.notesappmvvm.database.room.repository.RoomRepository
 import ru.tikodvlp.notesappmvvm.model.Note
+import ru.tikodvlp.notesappmvvm.utils.REPOSITORY
 import ru.tikodvlp.notesappmvvm.utils.TYPE_FIREBASE
 import ru.tikodvlp.notesappmvvm.utils.TYPE_ROOM
 import java.lang.IllegalArgumentException
 
 class MainViewModel (application: Application) : AndroidViewModel(application) {
 
-    val readTest: MutableLiveData<List<Note>> by lazy {
-        MutableLiveData<List<Note>>()
-    }
+    val context = application
 
-    val dbType: MutableLiveData<String> by lazy {
-        MutableLiveData<String>(TYPE_ROOM)
-    }
-
-    init {
-        readTest.value =
-            when(dbType.value) {
-                TYPE_ROOM -> {
-                    listOf<Note>(
-                        Note(title = "Note 1", subtitle = "Subtitle for note 1"),
-                        Note(title = "Note 2", subtitle = "Subtitle for note 2"),
-                        Note(title = "Note 3", subtitle = "Subtitle for note 3"),
-                        Note(title = "Note 4", subtitle = "Subtitle for note 4"),
-                    )
-                }
-                TYPE_FIREBASE -> listOf()
-                else -> listOf()
-            }
-    }
-
-    fun initDatabase(type: String) {
-        dbType.value = type
+    fun initDatabase(type: String, onSuccess: ()-> Unit) {
         Log.d("checkData", "MainViewModel initDatabase with $type")
+        when(type) {
+            TYPE_ROOM -> {
+                val dao = AppRoomDatabase.getInstance(context = context).getRoomDao()
+                REPOSITORY = RoomRepository(dao)
+                onSuccess()
+            }
+        }
     }
 }
 class MainViewModelFactory(private val application: Application) : ViewModelProvider.Factory {
